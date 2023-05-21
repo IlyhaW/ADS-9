c2dcacdf5c0102ff3590ebac7df7c592a271f4d6
IlyhaW <125214907+IlyhaW@users.noreply.github.com>
2023-05-21 13:56:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,31 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <cctype>
+#include <string>
 #include  "bst.h"
-
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> str;
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "Error." << std::endl;
+    return str;
+  }
+  std::string word = "";
+  while (!file.eof()) {
+    int ch = tolower(file.get());
+    if ((ch > 96 && ch < 123)) {
+      word += ch;
+    } else {
+      if (word != "")
+        str.add(word);
+      word = "";
+    }
+    if (ch == ' ') {
+      str.add(word);
+      word = "";
+    }
+  }
+  file.close();
+  return str;
 }
